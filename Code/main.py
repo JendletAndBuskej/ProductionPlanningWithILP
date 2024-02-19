cdf6995fb54c502cf07807c6010a094e1ef5b044
buskej <buskej@student.chalmers.se>
2024-02-19 17:10:46 +0100

    another run possibility and one constraint

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,7 @@
 import pyomo.environ as pyo
+import pandas as pd
+
+
 
 model = pyo.AbstractModel()
 
@@ -13,18 +16,9 @@ model.time_indices = pyo.RangeSet(1, model.num_time_indices)
 model.operation_valid_machine_types = pyo.Param(model.operations, model.machines)
 model.operation_time = pyo.Param(model.operations)
 
-# model.assigned = pyo.Var(model.machines, model.operations, model.time_indices, within=pyo.Binary)
 model.assigned = pyo.Var(model.machines, model.operations, model.time_indices, domain=pyo.Binary)
 
-
-# def obj_function(model):
-    # return pyo.Expression(expr=max(model.assigned[m,o,t] * t for m in model.machines for o in model.operations for t in model.time_indices))
-    # return pyo.max(model.assigned[m,o,t] * t for m in model.machines for o in model.operations for t in model.time_indices)
-# 
-# model.obj = pyo.Objective(rule=obj_function)
-
 def objective_rule(model):
-    #return sum(model.time_indices[t] * model.assigned[m, o, t] for (m, o, t) in model.assigned) 
     return sum(t * model.assigned[m, o, t] for m in model.machines for o in model.operations for t in model.time_indices)  
 
 model.objective = pyo.Objective(rule=objective_rule)
@@ -35,31 +29,20 @@ def no_duplicate_const(model, operation):
 
 model.no_duplicate = pyo.Constraint(model.operations, rule=no_duplicate_const)
 
+def machine_type_const(model, machine, operation):
+    return sum(model.assigned[machine,operation,t] for t in model.time_indices) <= model.operation_valid_machine_types[operation, machine]
+
+model.machine_type_const = pyo.Constraint(model.machines, model.operations, rule=machine_type_const)
+
+
+
 
 """
-model.m = pyo.Param(within=pyo.NonNegativeIntegers)
-model.n = pyo.Param(within=pyo.NonNegativeIntegers)
 
-model.I = pyo.RangeSet(1, model.m)
-model.J = pyo.RangeSet(1, model.n)
+instance = model.create_instance('Data/Pyomo/pyo_data.dat')
+opt = SolverFactory('glpk')
+result = opt.solve(instance)
 
-model.a = pyo.Param(model.I, model.J)
-model.b = pyo.Param(model.I)
-model.c = pyo.Param(model.J)
-
-# the next line declares a variable indexed by the set J
-model.x = pyo.Var(model.J, domain=pyo.NonNegativeReals)
-
-def obj_expression(m):
-    return pyo.summation(m.c, m.x)
-
-model.OBJ = pyo.Objective(rule=obj_expression)
-
-def ax_constraint_rule(m, i):
-    # return the expression for the constraint for i
-    return sum(m.a[i,j] * m.x[j] for j in m.J) >= m.b[i]
-
-# the next line creates one constraint for each member of the set model.I
-model.AxbConstraint = pyo.Constraint(model.I, rule=ax_constraint_rule)
-
+df = pd.DataFrame.from_dict(model.assigned.extract_values(), orient='index', columns=[str(model.assigned)])
+print(df)
 """
