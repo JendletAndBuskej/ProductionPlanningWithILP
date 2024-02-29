d9ce4f1c691dc39c666f7a73286aee977e23c4cb
buskej <buskej@student.chalmers.se>
2024-02-29 10:51:56 +0100

    added some to the environment

--- Code/environment.py
+++ Code/environment.py
@@ -1,7 +1,7 @@
 ############## IMPORT ################
 import numpy as np
 import matplotlib.pyplot as plt
-import os, json, re
+import os, json, re, math
 from classes import Operation, Order
 from Data_Converter.batch_data import Batch_Data
 import pyomo as pyo
@@ -16,6 +16,9 @@ class Environment:
         self.operations = self.initialize_operations()
         self.orders = self.initialize_orders()    # is it easy to make them ordered, from time low to high
         self.time = []
+        self.schedule = self.initial_schedule()
+        self.max_oper = 1
+        self.time_step_size = 50 # this is just before we fix this
         #self.schedule_matrix = self.initial_schedule(self.orders, len(self.operations), 
         #                                        len(self.machines))    #order id = index in matrix
         self.last_sent_indices = []  #list of machines, list of operations, time_interval 
@@ -35,8 +38,8 @@ class Environment:
         #self.schedule_matrix[2,1] = 1
         #self.schedule_matrix[2,2] = 2
         #self.to_ilp(unlocked_operations_indices=[1], locked_operations_indices=[0,2], time_interval=[0,2])
-
-    def unlock_order(self, amount, t_interval , order_id=[0]):
+    
+    def unlock_order(self, nr_orders, t_interval, order_idx=[0]):
         """this will return the input dict that ILP takes as input.
         it will unlock the amount of of operations given
 
@@ -46,13 +49,46 @@ class Environment:
             order_idx (list, optional): _description_. Defaults to [0].
         """
         #unlocked operations in interval, just for you Theo my friend, know these comments will be removed
-        sorted_id = id_handler(amount, order_id, len(self.operations) - 1)
-        part_schedule = self.schedule_matrix[:, sorted_id, t_interval[0]:t_interval[1]]
-        #locked operations in interval
-        locked_operations = self.schedule_matrix[:, :, t_interval[0]:t_interval[1]]
-        locked_operations = np.delete(locked_operations, sorted_id, 1)
-        self.last_sent_indices = [range(self.schedule_matrix.shape[0]), sorted_id, t_interval]
-        return(self.to_ilp(part_schedule, t_interval))#, locked_operations))
+        locked_oper = []
+        unlocked_oper = []
+        if (len(order_idx) == 1 and order_idx[0] == 0):
+            order_idx = np.ceil(len(self.orders)*np.random.rand(nr_orders))
+        for oper_idx in range(len(self.schedule[0])):
+            oper3 = [self.schedule[0][oper_idx],
+                     self.schedule[1][oper_idx],
+                     self.schedule[2][oper_idx]]
+            if (oper3[2] > t_interval[0] and oper3[2] < t_interval[1] - self.max_oper):
+                i = 0
+                for idx in order_idx:
+                    i += 1
+                    if (oper3[1].order == self.orders[idx]):
+                        unlocked_oper += [oper3[1]]
+                        break
+                    if (i == len(order_idx)):
+                        locked_oper += [oper3[1]]
+            elif (oper3[2] > t_interval[1] -self.max_oper and oper3[2] < t_interval[1]):
+                exe_time = ceil(oper3[1].execution_time/self.time_step_size)
+                if (oper3[2] + exe_time <= t_interval[1]):
+                    unlocked_oper += [oper3[1]]
+        locked_oper += self.line_check(t_interval[0])
+        locked_oper += self.line_check(t_interval[1])
+        return(unlocked_oper, locked_oper)
+
+    def line_check(self, time_of_line):
+        oper_on_line = []
+        range_set = [time_of_line - self.max_oper, time_of_line]
+        if (time_of_line - self.max_oper < 0):
+            range_set = [0, time_of_line]
+        for oper_idx in range(len(self.schedule[0])):
+            oper3 = [self.schedule[0][oper_idx],
+                     self.schedule[1][oper_idx],
+                     self.schedule[2][oper_idx]]
+            check1 = oper3[2] > range_set[0] and oper3[2] > range_set[1]
+            exe_time = ceil(oper3[1].execution_time/self.time_step_size)
+            check2 = oper3[2] + exe_time > time_of_line
+            if (check1 and check2):
+                oper_on_line += [oper3[1]]
+        return(oper_on_line)
 
     def unlock_machine(self, amount, t_interval, machine_id=[0]):
         sorted_id = id_handler(amount, machine_id, len(self.machines) - 1)
@@ -146,41 +182,42 @@ class Environment:
 
     
     ############# HELP_FUNCTIONS ###############
-    def initial_schedule(self, orders, num_operations, num_machines):
+    def initial_schedule(self):
         """this will return a semi-bad schedule that dosn't break
         any constraints. It assumes that all operations is of the
         same length and that order.get_operations() is sorted so that 
         placing the operations in that order wont break precedence constraint.
         """
-        def find_space(operation, min_time, schedule, schedule2d):
-            if (min_time < schedule.shape[2]):
-                for time in range(min_time, schedule.shape[2]):
-                    for machine in operation.get_valid_machines():
+        def find_space(operation, min_time, schedule, schedule_2d):
+            if (min_time < schedule_2d.shape[1]):
+                for time in range(min_time, schedule_2d.shape[1]):
+                    for machine in operation.get_valid_machine_ids():
                         if (schedule_2d[machine, time] == 0):
                             min_time = time + 1
                             schedule_2d[machine, time] = 1
-                            schedule[machine, operation, time] = 1
-                            return(min_time, schedule, schedule2d)
-            min_time = schedule.shape[2]
-            np.append(schedule2d, np.zeros(schedule2d.shape[0]), axis=1)
-            np.append(schedule, np.zeros(schedule.shape[0:1]), axis=2)
-            return(find_space(operation, min_time, schedule, schedule2d))
+                            schedule[0].append(machine)
+                            schedule[1].append(operation) 
+                            schedule[2].append(time) 
+                            return(min_time, schedule, schedule_2d)
+            min_time = schedule_2d.shape[1]
+            schedule_2d = np.append(schedule_2d, np.zeros([schedule_2d.shape[0],1]), axis=1)
+            return(find_space(operation, min_time, schedule, schedule_2d))
 
-        schedule = np.zeros([num_machines, num_operations, 1])
-        schedule_2d = np.zeros([num_machines, 1])
-        for order in orders:
+        schedule = [[],[],[]]
+        schedule_2d = np.zeros([len(self.machines), 1])
+        for order in self.orders:
             min_time = 0
             for operation in order.get_operations():
-                schedule, min_time, schedule_2d = find_space(operation, min_time,
+                min_time, schedule, schedule_2d = find_space(operation, min_time,
                                                             schedule, schedule_2d)
         return(schedule)
     
     def to_ilp(
-               self, 
-               unlocked_operations_indices: list[int], 
-               locked_operations_indices: list[int],
-               time_interval: list[int]
-            ) -> dict:
+            self, 
+            unlocked_operations_indices: list[int], 
+            locked_operations_indices: list[int],
+            time_interval: list[int]
+        ) -> dict:
         """Converts the data of the schedule to send to the ILP. Includes a list of operation index
         to optimize and a list of operation index to keep locked. Returns a dict
 
@@ -286,3 +323,4 @@ if (__name__ == "__main__"):
     batch_data = Batch_Data(batch_size=batch_size)
     batched_data = batch_data.get_batch()
     env = Environment(batched_data)
+    test1, test2 = env.unlock_order(1, [0,10])
