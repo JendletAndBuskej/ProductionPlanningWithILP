4e8bdc33efacf5eb21731fc736c55b6de52c7c46
buskej <buskej@student.chalmers.se>
2024-03-02 20:13:47 +0100

    cleaned up ilp file and added precendence constraint for locked scedules. also split the function for creating and running the model for slightly better performence

--- Code/ilp.py
+++ Code/ilp.py
@@ -1,40 +1,60 @@
 ########### IMPORTS #################
-from platform import machine
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
+### IMPORTS_FOR_TEST ###
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-def create_and_run_ilp(ilp_data : dict | str):
-    ############ CONSTRUCTING MODEL ################
-    # parameters and variable definition
+def create_ilp(weight_json: dict | str = {}):
+    """This function constructs an abstract ILP model of our problem.
+    This constructions is done with a set of constrains and objective
+    function parts that can be weighted in an json to get an desired result.
+
+    Args:
+    weight_json (dict | str): If specified this will change the weights on the objective
+    functions.
+
+    Returns:
+        pyo.AbstractModel: an abstract ILP model.
+    """
+    if (len(weight_json) == 0):
+        weight_json = {
+            "makeSpan": 1
+        }
     model = pyo.AbstractModel()
+    # CONSTANTS
+    BIG_M = 10000
     model.num_machines = pyo.Param(within=pyo.NonNegativeIntegers)
     model.num_opers = pyo.Param(within=pyo.NonNegativeIntegers)
     model.num_locked_opers = pyo.Param(within=pyo.NonNegativeIntegers)
     model.num_time_indices = pyo.Param(within=pyo.NonNegativeIntegers)
+    # RANGES
     model.machines = pyo.RangeSet(1, model.num_machines)
     model.opers = pyo.RangeSet(1, model.num_opers)
     model.locked_opers = pyo.RangeSet(1, model.num_locked_opers)
-    model.all_opers = pyo.RangeSet(1,model.num_opers+model.num_locked_opers)
     model.time_indices = pyo.RangeSet(1, model.num_time_indices)
-    model.valid_machines = pyo.Param(model.opers, model.machines)
-    model.precedence = pyo.Param(model.all_opers, model.all_opers)
+    # PARAMETER
+    model.valid_machines = pyo.Param(model.opers, 
+                                     model.machines)
+    model.precedence = pyo.Param(model.opers, 
+                                 model.opers)
+    model.locked_prece_before = pyo.Param(model.locked_opers, 
+                                          model.opers)
+    model.locked_prece_after = pyo.Param(model.opers, 
+                                         model.locked_opers)
     model.exec_time = pyo.Param(model.opers)
     model.locked_exec_time = pyo.Param(model.locked_opers)
-    model.locked_schedule = pyo.Param(model.machines, model.locked_opers, model.time_indices)
-    model.assigned = pyo.Var(model.machines, model.opers, model.time_indices, domain=pyo.Binary)
-    big_m = 10000
+    model.locked_schedule = pyo.Param(model.machines, 
+                                      model.locked_opers, 
+                                      model.time_indices)
+    # VARIABLE
+    model.assigned = pyo.Var(model.machines, 
+                             model.opers, 
+                             model.time_indices, 
+                             domain=pyo.Binary)
 
-
-    # objective function and constraints
-    def objective(model):
-        return sum(t * model.assigned[m, o, t] 
-                   for m in model.machines
-                   for o in model.opers
-                   for t in model.time_indices)  
-
+    #################### CONSTRAINTS ########################
     def duplicate_const(model, oper):
         return sum(model.assigned[m,oper,t]
                    for m in model.machines 
@@ -42,55 +62,128 @@ def create_and_run_ilp(ilp_data : dict | str):
 
     def machine_const(model, machine, oper):
         valid_machine = model.valid_machines[oper, machine]
+        if (valid_machine == 1):
+            return pyo.Constraint.Skip
         return sum(model.assigned[machine,oper,t] 
-                for t in model.time_indices) <= valid_machine
+                   for t in model.time_indices) <= valid_machine
 
     def overlap_const(model, machine, oper, time_index):
+        if (time_index == model.time_indices[-1]):
+            return(pyo.Constraint.Skip)
         start_interval = min(time_index, model.time_indices[-2])
         end_interval = min(model.exec_time[oper] + time_index, model.time_indices[-1])
         time_interval = range(start_interval, end_interval) 
         locked_schedule = model.assigned[machine, oper, time_index]      
         return sum(model.assigned[machine, o, t] 
                    for o in model.opers 
-                   for t in time_interval) <= 1 + big_m*(1-locked_schedule)
+                   for t in time_interval) <= 1 + BIG_M*(1-locked_schedule)
 
     def locked_overlap_const(model, machine, locked_oper, time_index):
         if (time_index == model.time_indices[-1]):
             return(pyo.Constraint.Skip)
         start_interval = min(time_index, model.time_indices[-2])
-        end_interval = min(model.locked_exec_time[locked_oper] + time_index, model.time_indices[-1])
+        end_interval = min(model.locked_exec_time[locked_oper] 
+                           + time_index, model.time_indices[-1])
         time_interval = range(start_interval, end_interval) 
-        locked = model.locked_schedule[machine, locked_oper, time_index]      
+        locked = model.locked_schedule[machine, locked_oper, time_index]
         return sum(model.assigned[machine, o, t] 
                    for o in model.opers 
-                   for t in time_interval) <= big_m*(1-locked)
+                   for t in time_interval) <= BIG_M*(1-locked)
     
     def precedence_const(model, oper, other_oper):
-        if (oper == other_oper):
+        precedence = model.precedence[oper,other_oper]
+        if (precedence == 0 or oper == other_oper):
             return(pyo.Constraint.Skip)
         start_time_oper = sum(t*model.assigned[m, other_oper, t] 
-                             for m in model.machines 
-                             for t in model.time_indices)
-        end_time_sum = sum((t + model.exec_time[oper])*model.assigned[m, oper, t]
-                             for m in model.machines 
-                             for t in model.time_indices)
-        end_time_other_oper = end_time_sum*model.precedence[oper,other_oper]
-        return(start_time_oper >= end_time_other_oper)
-
-    model.objective = pyo.Objective(rule=objective)
-    model.no_duplicate = pyo.Constraint(model.opers, rule=duplicate_const)
-    model.machine_const = pyo.Constraint(model.machines, model.opers, rule=machine_const)
-    model.overlap_const = pyo.Constraint(model.machines, model.opers, model.time_indices, rule=overlap_const)
-    model.locked_overlap_const = pyo.Constraint(model.machines, model.locked_opers, model.time_indices, rule=locked_overlap_const)
-    #model.precedence_const = pyo.Constraint(model.opers, model.opers, rule=precedence_const)
+                              for m in model.machines 
+                              for t in model.time_indices)
+        end_time_sum = sum((t + model.exec_time[oper])*model.assigned[m, oper, t] 
+                           for m in model.machines 
+                           for t in model.time_indices)
+        end_time_other_oper = end_time_sum*precedence
+        return(start_time_oper >= end_time_other_oper + 1)
     
-    ############# SOLVE ############
+    def locked_prece_before_const(model, locked_oper, oper):
+        precedence = model.locked_prece_before[locked_oper,oper]
+        if (precedence == 0):
+            return(pyo.Constraint.Skip)
+        start_time_locked = sum(t*model.locked_schedule[m, locked_oper, t] 
+                                for m in model.machines 
+                                for t in model.time_indices)
+        end_time_sum = sum((t + model.exec_time[oper])*model.assigned[m, oper, t] 
+                           for m in model.machines 
+                           for t in model.time_indices)
+        end_time_other_oper = end_time_sum*precedence
+        return(start_time_locked >= end_time_other_oper + 1)
+    
+    def locked_prece_after_const(model, oper, locked_oper):
+        precedence = model.locked_prece_after[oper,locked_oper]
+        if (precedence == 0):
+            return(pyo.Constraint.Skip)
+        start_time_oper = sum(t*model.assigned[m, oper, t] 
+                              for m in model.machines 
+                              for t in model.time_indices)
+        exec_time = model.locked_exec_time[locked_oper]
+        end_time_sum = sum((t + exec_time)*model.locked_schedule[m, locked_oper, t]
+                           for m in model.machines 
+                           for t in model.time_indices)
+        end_time_locked_oper = end_time_sum*precedence
+        return(start_time_oper >= end_time_locked_oper + 1)
+
+    ################ OBJECTIVE_FUNCTION ######################
+    def objective(model):
+        def makeSpan():
+            return sum(t * model.assigned[m, o, t] 
+                       for m in model.machines 
+                       for o in model.opers 
+                       for t in model.time_indices)
+        
+        makeSpan = weight_json["makeSpan"]*makeSpan()
+        return(makeSpan)
+
+    ############## SET_MODEL ###############
+    model.objective = pyo.Objective(rule=objective)
+    model.no_duplicate = pyo.Constraint(model.opers, 
+                                        rule=duplicate_const)
+    model.machine_const = pyo.Constraint(model.machines, 
+                                         model.opers, 
+                                         rule=machine_const)
+    model.overlap_const = pyo.Constraint(model.machines, 
+                                         model.opers, 
+                                         model.time_indices, 
+                                         rule=overlap_const)
+    model.locked_overlap_const = pyo.Constraint(model.machines, 
+                                                model.locked_opers, 
+                                                model.time_indices, 
+                                                rule=locked_overlap_const)
+    model.precedence_const = pyo.Constraint(model.opers, 
+                                            model.opers, 
+                                            rule=precedence_const)
+    model.locked_prece_before_const = pyo.Constraint(model.locked_opers, 
+                                                     model.opers, 
+                                                     rule=locked_prece_before_const)
+    model.locked_prece_after_const = pyo.Constraint(model.opers, 
+                                                    model.locked_opers, 
+                                                    rule=locked_prece_after_const)
+    return(model)
+
+def run_ilp(model, ilp_data : dict | str):
+    """This function runs an abstract model with given instance data
+    and returns the solved instance if the model.
+
+    Args:
+        model (pyo.AbstractModel): An abstract model.
+        ilp_data (dict | str): The instance data.
+    """
     instance = model.create_instance(ilp_data)
     opt = SolverFactory("glpk")
-    result = opt.solve(instance)
+    opt.solve(instance)
     return(instance)
 
 
+#####################################
+################ TEST #####################
+#####################################
 if (__name__ == "__main__"):
     # https://readthedocs.org/projects/pyomo/downloads/pdf/stable/   
     # SIDA 69/743 för att se hur den skrivs
@@ -122,9 +215,9 @@ if (__name__ == "__main__"):
                 3: 1,
             },
             "locked_schedule" : {
-                (1,1,1): 1,
+                (1,1,1): 0,
                 (1,1,2): 0,
-                (1,1,3): 0,
+                (1,1,3): 1,
                 (1,1,4): 0,
                 (1,1,5): 0,
                 (2,1,1): 0,
@@ -134,23 +227,34 @@ if (__name__ == "__main__"):
                 (2,1,5): 0
             },
             "locked_exec_time" : {
-                1: 2
+                1: 1
             },
             "precedence" : {
-                (1,1): 1,
-                (1,2): 1,
-                (1,3): 1,
+                (1,1): 0,
+                (1,2): 0,
+                (1,3): 0,
                 (2,1): 0,
-                (2,2): 1,
+                (2,2): 0,
                 (2,3): 0,
                 (3,1): 0,
-                (3,2): 1,
+                (3,2): 0,
+                (3,3): 0,
+            },
+            "locked_prece_before" : {
+                (1,1): 1,
+                (1,2): 0,
+                (1,3): 0,
+            },
+            "locked_prece_after" : {
+                (1,1): 0,
+                (2,1): 1,
+                (3,1): 0,
             }
-
         }
     }
     
-    instance = create_and_run_ilp(test_dict)
+    ilp_model = create_ilp()
+    instance = run_ilp(ilp_model, test_dict)
     #instance = create_and_run_ilp("Data/Pyomo/pyo_data.dat")
     # data management
     def instance_2_numpy(instance_data: pyo.Var | pyo.Param | pyo.Set | pyo.RangeSet, 
